8d1d7ffb7a977c50d42b9f65d6d2fa5599021fae
Slimikus <slimikus@gmail.com>
2016-03-13 17:15:56 +0300

    пример цикла for перебор массива

--- Повторения 10-1.playground/Contents.swift
+++ Повторения 10-1.playground/Contents.swift
@@ -1,5 +1,15 @@
+for var i=0; i<5; i++ {
+    print("i =  \(i)")
+}
 
+for var f = 0 ; f < 5; ++f {
+    f++
+    print("f = ",f)
+}
 
-
+var myArray = ["RUS", "USA", "BY"]
+for var i = 0; i < myArray.count; i++ {
+    print(myArray[i])
+}
 
 
