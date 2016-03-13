6cb563a96ae6e49dd977743fca95fc7e5f9a6bda
Slimikus <slimikus@gmail.com>
2016-03-13 17:42:46 +0300

    + обход словоря

--- Повторения 10-1.playground/Contents.swift
+++ Повторения 10-1.playground/Contents.swift
@@ -12,4 +12,13 @@ for var i = 0; i < myArray.count; i++ {
     print(myArray[i])
 }
 
+var mySports = ["Football": "Футбол", "Polo": "Поло", "Golf": "Гольф"]
+// получаем массив всех ключей
+let mySportsArray = Array(mySports.keys)
+/* обходим массив ключей
+для доступа к значениям словоря */
+for var i=0; i < mySportsArray.count; i++ {
+    print(mySports[mySportsArray[i]]!)
+}
+
 
