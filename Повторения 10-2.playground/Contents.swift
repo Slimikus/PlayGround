b9f8735041de9da4a91926fbb64e731cb88c1bf0
Slimikus <slimikus@gmail.com>
2016-03-17 09:48:23 +0300

    Добавлены операторы repeat while и continue

--- Повторения 10-2.playground/Contents.swift
+++ Повторения 10-2.playground/Contents.swift
@@ -8,6 +8,26 @@ while i <= 10 {
 }
 resultSum
 
+// Оператор Repear while
+
+i = 1
+resultSum = 0
+repeat {
+    resultSum += 1
+    i++
+} while i <= 10
+resultSum
+
+// Оператор Continue
+
+for i in 1...10 {
+    if i%2 == 0 {
+        continue
+    } else {
+        print(i)
+    }
+}
+
 
 
 
