3810ae41d5140e7bc27167e43b15962d396edc3f
Slimikus <slimikus@gmail.com>
2016-03-18 14:06:27 +0300

    Добавлены примеры вызова функции

---
+++ Функции 11.playground/Contents.swift
@@ -0,0 +1,18 @@
+// Функции
+
+func printMessage1() -> Void {
+    print("Сообщение 1 принято")
+}
+printMessage1()
+
+func printMessage2() -> () {
+    print("Сообщение 2 принято")
+}
+printMessage2()
+
+func printMessage3() {
+    print("Сообщение 3 принято")
+}
+printMessage3()
+
+
