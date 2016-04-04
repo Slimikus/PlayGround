d04410420f820dec2717a6c3266a757168df1d18
Slimikus <slimikus@gmail.com>
2016-04-04 16:58:02 +0300

    14.2 Ассоциированные параметры

--- Перечисления 14.playground/Contents.swift
+++ Перечисления 14.playground/Contents.swift
@@ -11,11 +11,52 @@ enum CurrencyUnit2 {
 }
 
 //способ 1
-let ruobleCurrency = CurrencyUnit1.Rouble
+let ruobleCurrency1 = CurrencyUnit1.Rouble
 //способ 2
-let dollarCurrency: CurrencyUnit2
-dollarCurrency = .Dollar
+let dollarCurrency1: CurrencyUnit2
+dollarCurrency1 = .Dollar
 
 // 14.2 Ассоциированные параметры
 
+enum CurrencyUnit3 {
+    case Rouble(countrys: [String], shortName: String)
+    case Dollar(countrys: [String], shortName: String)
+    case Euro(countrys: [String], shotName: String)
+}
+
+var roubleCurrency2: CurrencyUnit3
+roubleCurrency2 = .Rouble(countrys: ["Russia"], shortName: "RUB")
+
+//Вариант 1
+//страны использующие доллар
+enum DollarCountrys {
+    case USA
+    case Canada
+    case Australia
+}
+//тип данных "валюта"
+enum CurrencyUnit4 {
+    case Rouble(countrys: [String], shortName: String)
+    case Dollar(countrys: [String], shortName: String, national: DollarCountrys)
+    case Euro(countrys: [String], shotName: String)
+}
+var dollarCurrency2: CurrencyUnit4
+dollarCurrency2 = .Dollar(countrys: ["USA"], shortName: "USD", national: .USA)
+
+//Вариант 2
+//тип данных "валюта"
+enum CurrencyUnit5 {
+    //страны использующие доллар
+    enum DollarCountrys {
+        case USA
+        case Canada
+        case Australia
+    }
+    case Rouble(countrys: [String], shortName: String)
+    case Dollar(countrys: [String], shortName: String, national: DollarCountrys)
+    case Euro(countrys: [String], shotName: String)
+}
+
+var someVar: CurrencyUnit5.DollarCountrys
+someVar = .Australia
 
