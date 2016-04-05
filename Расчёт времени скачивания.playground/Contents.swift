e89c6f28c0bd6dac30acd16bbd0f8e21409ef735
Slimikus <slimikus@gmail.com>
2016-04-05 17:02:53 +0300

    + Добавлен подсчёт дней

--- Расчёт времени скачивания.playground/Contents.swift
+++ Расчёт времени скачивания.playground/Contents.swift
@@ -10,7 +10,7 @@ var den: Int
 skorSoed.0 = 199
 skorSoed.1 = "kbyte"
 skorSoed
-obyemSkach.0 = 15000
+obyemSkach.0 = 150000
 obyemSkach.1 = "Mb"
 obyemSkach
 
@@ -60,6 +60,13 @@ func vremyaSkach (sec: Int) {
         tMin = min - tMin
         return (tMin, chas)
     }
+    // Подсчёт дней
+    func fDni(tChas: Int) -> (tChas: Int, dni: Int) {
+        let dni = chas / 24
+        var tChas = dni * 24
+        tChas = chas - tChas
+        return (tChas, dni)
+    }
     
     skorSoed.0 = skorSoed.0 * 1024
     if obyemSkach.0 < skorSoed.0 {
@@ -68,16 +75,24 @@ func vremyaSkach (sec: Int) {
         switch sec {
         case  0...59: //секунды
             print("Время скачивания \(sec) секунд(-а)(-ы)")
-        case 60...3599: //минуты
+        case 60...3_599: //минуты
             sec
             let request = fMin(sec)
             print("Время скачивания \(request.min) минут(-а)(-ы) \(request.tSec) секунд(-а)(-ы)")
-        case 3600...86399: //часы
+        case 3_600...86_399: //часы
             sec
             let request1 = fMin(sec)
             min = request1.min
             let request2 = fChas(request1.min)
             print("Время скачивания \(request2.chas) час(-а)(-ов) \(request2.tMin) минут(-а)(-ы) \(request1.tSec) секунд(-а)(-ы)")
+        case 86400...2_073_600:
+            sec
+            let request1 = fMin(sec)
+            min = request1.min
+            let request2 = fChas(request1.min)
+            chas = request2.chas
+            let request3 = fDni(request2.chas)
+            print("Время скачивания \(request3.dni) день(дней) \(request3.tChas) час(-а)(-ов) \(request2.tMin) минут(-а)(-ы) \(request1.tSec) секунд(-а)(-ы)")
         default:
             break
         }
