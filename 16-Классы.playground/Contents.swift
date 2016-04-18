058fd30a9d41b366333e69773b41b39ef5ee648e
Slimikus <slimikus@gmail.com>
2016-04-18 13:39:23 +0300

    16.4 Инициализаторы классов

--- 16-Классы.playground/Contents.swift
+++ 16-Классы.playground/Contents.swift
@@ -59,6 +59,33 @@ class Chessman3 {
 var kingWhite2 = Chessman3(type: .King, color: .White, figure: "♔")
 kingWhite2.setCoordinates(char: "E", num: 1)
 
+// 16.4 Инициализаторы классов
+
+class Chessman4 {
+    let type: ChessmanType
+    let color: ChessmanColor
+    var coordinates: (String, Int)? = nil
+    let figureSymbol: Character
+    init(type: ChessmanType, color: ChessmanColor, figure: Character){
+        self.type = type
+        self.color = color
+        self.figureSymbol = figure
+    }
+    init(type: ChessmanType, color: ChessmanColor, figure: Character, coordinates: (String, Int)){
+        self.type = type
+        self.color = color
+        self.figureSymbol = figure
+        self.coordinates = (char: coordinates.0, num: coordinates.1)
+    }
+    func setCoordinates(char c: String, num n: Int) {
+        self.coordinates = (c, n)
+    }
+    func  kill() {
+        self.coordinates = nil
+    }
+}
+var QueenBlack = Chessman4(type: .Queen, color: .Black, figure: "♛", coordinates: ("A", 6))
+
 
 
 
