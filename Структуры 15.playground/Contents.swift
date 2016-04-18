9338fd07cd8b22b1b5cc1e9597c631b5cc59dd47
Slimikus <slimikus@gmail.com>
2016-04-18 11:39:06 +0300

    15.2 Свойства в структурах

--- Структуры 15.playground/Contents.swift
+++ Структуры 15.playground/Contents.swift
@@ -2,12 +2,25 @@
 
 // 15.1 Синтаксис объявления структур
 
-struct PlayerInChess {}
-var oleg: PlayerInChess
+struct PlayerInChess0 {}
+var oleg0: PlayerInChess0
 
 // 15.2 Свойства в структурах
 
+struct PlayerInChess1 {
+    var name: String
+    var wins: UInt
+}
 
+var oleg1 = PlayerInChess1(name: "Олег", wins: 32)
+
+struct PlayerInchess2 {
+    var name = ""
+    var wins: UInt = 0
+}
+
+var oleg2 = PlayerInchess2(name: "Олег", wins: 33)
+var maks = PlayerInchess2()
 
 
 
