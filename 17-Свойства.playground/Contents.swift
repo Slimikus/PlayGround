3ac5155b6524cda6082dfa953ee89f0f001f1731
Slimikus <slimikus@gmail.com>
2016-04-18 14:37:36 +0300

    17.2 Контроль получения и установки значений

--- 17-Свойства.playground/Contents.swift
+++ 17-Свойства.playground/Contents.swift
@@ -21,3 +21,72 @@ class AboutMan2 {
 var Me2 = AboutMan2()
 Me2.wholeName
 
+// 17.2 Контроль получения и установки значений
+
+// Геттер и сеттер
+struct Circle {
+    var coordinates: (x: Int, y: Int)
+    var radius: Float
+    var perimeter: Float {
+        get {
+            return 2.0 * 3.14 * self.radius
+        }
+        set(newPerimeter){
+            self.radius = newPerimeter / (2 * 3.14)
+        }
+    }
+}
+var myNewCircle = Circle(coordinates: (0, 0), radius: 10)
+myNewCircle.perimeter
+myNewCircle.perimeter = 31.4
+myNewCircle.radius
+
+struct Circle2 {
+    var coordinates: (x: Int, y: Int)
+    var radius: Float
+    var perimeter: Float {
+        get {
+            return 2.0 * 3.14 * self.radius
+        }
+        set{
+            self.radius = newValue / (2 * 3.14)
+        }
+    }
+}
+var myNewCircle2 = Circle2(coordinates: (0, 0), radius: 14)
+myNewCircle2.perimeter
+myNewCircle2.perimeter = 90
+myNewCircle2.radius
+
+// Наблюдатели
+struct Circle3 {
+    var coordinates: (x: Int, y: Int)
+    var radius: Float {
+        willSet (newValueOfRadius) {
+            print("Вместо значения \(self.radius) устанавливается значение \(newValueOfRadius)")
+        }
+        didSet (oldValueOfRadius) {
+            print("Вместо значения \(oldValueOfRadius) установлено значение \(self.radius)")
+        }
+    }
+    var perimeter: Float {
+        get {
+            return 2.0 * 3.14 * self.radius
+        }
+        set{
+            self.radius = newValue / (2 * 3.14)
+        }
+    }
+}
+var myNewCircle3 = Circle3(coordinates: (0, 0), radius: 10)
+myNewCircle3.perimeter
+myNewCircle3.perimeter = 94.2
+myNewCircle3.radius
+
+
+
+
+
+
+
+
