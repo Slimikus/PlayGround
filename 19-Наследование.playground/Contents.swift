39d0d19b99ef5e041d0e650c9751f774db3b070b
Slimikus <slimikus@gmail.com>
2016-04-27 15:26:28 +0300

    19.1 Синтаксис наследования

--- 19-Наследование.playground/Contents.swift
+++ 19-Наследование.playground/Contents.swift
@@ -1,5 +1,39 @@
 // 19 - Наследование
 
+// 19.1 Синтаксис наследования
+
+// суперкласс
+class Quadruped {
+    var type = ""
+    var name = ""
+    func walk(){
+        print("walk")
+    }
+}
+// подкласс
+class Dog: Quadruped {
+    func bark() {
+        print("woof")
+    }
+}
+var dog = Dog()
+dog.type = "dog"
+dog.walk()
+dog.bark()
+
+class Dog2: Quadruped {
+    func bark(){
+        print("woof")
+    }
+    func printName(){
+        print(self.name)
+    }
+}
+var dog2 = Dog2()
+dog2.name = "Dragon Wan Helsing"
+dog2.printName()
+
+
 
 
 
