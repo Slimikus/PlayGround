7875aff65fd85fae0f06a5731588fda900c90856
Slimikus <slimikus@gmail.com>
2016-04-29 09:43:23 +0300

    22.2 Уничтожение экземпляров

--- 22-Удаление экземпляров и ARC.playground/Contents.swift
+++ 22-Удаление экземпляров и ARC.playground/Contents.swift
@@ -1,2 +1,39 @@
 //: 22 - Удаление экземпляров и ARC
 
+// 22.1 Уничтожение экземпляров
+
+//Область видимости
+class myClass{
+    var description: String
+    init(description: String){
+        print("Экземпляр \(description) создан")
+        self.description = description
+    }
+    deinit{
+        print("Экземпляр \(description) уничтожен")
+    }
+}
+var myVar11 = myClass(description: "Один")
+if true {
+    var myVar12 = myClass(description: "Два")
+}
+
+//Количество ссылок на экземпляр
+class myClass2{
+    var description: String
+    init(description: String){
+        print("Экземпляр \(description) создан")
+        self.description = description
+    }
+    deinit{
+        print("Экземпляр \(description) уничтожен")
+    }
+}
+var myVar21 = myClass2(description: "Один")
+var myVar22 = myVar21
+myVar21 = myClass2(description: "Два")
+myVar22 = myVar21
+
+
+
+
