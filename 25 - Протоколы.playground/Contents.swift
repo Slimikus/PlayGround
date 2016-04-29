e65af8df021ccbeab60f0f4ef2c0edc3c3ffdbad
Slimikus <slimikus@gmail.com>
2016-04-29 15:59:34 +0300

    25.1 Требуемые свойства

--- 25 - Протоколы.playground/Contents.swift
+++ 25 - Протоколы.playground/Contents.swift
@@ -1,3 +1,22 @@
 //: 25 - Протоколы
 
+// 25.1 Требуемые свойства
+
+protocol SomeProtocol {
+    var mustBeSettable: Int {get set}
+    var doesNotNeedToBeSettable: Int {get}
+}
+
+protocol AnotherProtocol {
+    static var someTypeProperty: Int {get set}
+}
+
+protocol FullyNamed {
+    var fullName: String {get}
+}
+struct Person: FullyNamed {
+    var fullName: String
+}
+let John = Person(fullName: "John Wick")
+
 
