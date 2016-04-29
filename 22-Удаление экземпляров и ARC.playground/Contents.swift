fa463458f32804265f7386d88873ce62b1eabd7c
Slimikus <slimikus@gmail.com>
2016-04-29 10:12:20 +0300

    22.2 Утечка памяти

--- 22-Удаление экземпляров и ARC.playground/Contents.swift
+++ 22-Удаление экземпляров и ARC.playground/Contents.swift
@@ -34,6 +34,55 @@ var myVar22 = myVar21
 myVar21 = myClass2(description: "Два")
 myVar22 = myVar21
 
+// 22.2 Утечка памяти
+
+//Пример утечки памяти
+
+class Human {
+    let name: String
+    var child = [Human?]()
+    var father: Human?
+    var mother: Human?
+    init(name: String){
+        self.name = name
+    }
+    deinit{
+        print(self.name+" удалён")
+    }
+}
+if 1==1 {
+    var Kirill = Human(name: "Кирилл")
+    var Olga = Human(name: "Ольга")
+    var Aleks = Human(name: "Алексей")
+    Kirill.father = Aleks
+    Kirill.mother = Olga
+    Aleks.child.append(Kirill)
+    Olga.child.append(Kirill)
+}
+
+//Сильные и слабые ссылки
+class Human2 {
+    let name: String
+    var child = [Human2?]()
+    weak var father: Human2?
+    weak var mother: Human2?
+    init(name: String){
+        self.name = name
+    }
+    deinit{
+        print(self.name+" - удалён")
+    }
+}
+if 1==1 {
+    var Kirill = Human2(name: "Кирилл")
+    var Olga = Human2(name: "Ольга")
+    var Aleks = Human2(name: "Алексей")
+    Kirill.father = Aleks
+    Kirill.mother = Olga
+    Aleks.child.append(Kirill)
+    Olga.child.append(Kirill)
+}
+
 
 
 
