8540ca27807e94ecdbd5b26d4f87b26a7aaf4e6a
Slimikus <slimikus@gmail.com>
2016-05-05 15:20:12 +0300

    27.1 Универсальные функции

--- 27-Универсальные шаблоны.playground/Contents.swift
+++ 27-Универсальные шаблоны.playground/Contents.swift
@@ -1,3 +1,25 @@
 //: 27 - Универсальные шаблоны
 
+// 27.1 Универсальные функции
 
+func swapTwoInts(inout a: Int, inout _ b: Int) {
+    let tempA = a
+    a = b
+    b = tempA
+}
+var firstInt = 4010
+var secontInt = 13
+swapTwoInts(&firstInt, &secontInt)
+secontInt
+
+func swapTwoValue<T>(inout a: T, inout _ b: T) {
+    let tempA = a
+    a = b
+    b = tempA
+}
+var firstString = "one"
+var secondString = "two"
+swapTwoValue(&firstString, &secondString)
+secondString
+swapTwoValue(&firstInt, &secontInt)
+secontInt
