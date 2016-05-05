a946204d255e793a598a7851993c1d5c4728395f
Slimikus <slimikus@gmail.com>
2016-05-05 15:44:48 +0300

    27.2 Универсальные типы

--- 27-Универсальные шаблоны.playground/Contents.swift
+++ 27-Универсальные шаблоны.playground/Contents.swift
@@ -23,3 +23,46 @@ swapTwoValue(&firstString, &secondString)
 secondString
 swapTwoValue(&firstInt, &secontInt)
 secontInt
+
+// 27.2 Универсальные типы
+
+struct IntStack {
+    var items = [Int]()
+    mutating func push(item: Int) {
+        items.append(item)
+    }
+    mutating func pop() -> Int {
+        return items.removeLast()
+    }
+}
+
+struct IntStack2<T> {
+    var items = [T]()
+    mutating func push(item: T) {
+        items.append(item)
+    }
+    mutating func pop() -> T {
+        return items.removeLast()
+    }
+}
+var stackString = IntStack2<String>()
+stackString.push("uno")
+stackString.push("dos")
+let fromTheTop = stackString.pop()
+
+struct Stack<T> {
+    var items = [T]()
+    init(){}
+    init(_ elements: T...){
+        self.items = elements
+    }
+    mutating func push(item: T) {
+        items.append(item)
+    }
+    mutating func pop() -> T {
+        return items.removeLast()
+    }
+}
+var stackOfInt3 = Stack(1, 2, 4, 5)
+var stackOfString3 = Stack<String>()
+
