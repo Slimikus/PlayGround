99bb7e4a8f6c5f84a7ee93de6288fd8c49d35ec6
Slimikus <slimikus@gmail.com>
2016-05-06 11:26:28 +0300

    28.2 Обработка ошибок

--- 28-Обработка ошибок.playground/Contents.swift
+++ 28-Обработка ошибок.playground/Contents.swift
@@ -9,4 +9,65 @@ enum VendingMachineError: ErrorType {
 }
 throw VendingMachineError.InsufficientFunds(coinsNeeded: 5)
 
+// 28.2 Обработка ошибок
+
+// Передача ошибки
+
+struct Item {
+    var price: Int
+    var count: Int
+}
+class VendingMachine {
+    var inventory = [
+        "Candy Bar": Item(price: 12, count: 7),
+        "Chips": Item(price: 10, count: 4),
+        "Pretzels": Item(price: 7, count: 11)
+    ]
+    var coinsDeposites = 0
+    func dispenseSnack(snack: String) {
+        print("Dispensing \(snack)")
+    }
+    func vend(itemNamed name: String) throws {
+        guard var item = inventory[name] else {
+            throw VendingMachineError.InvalidSelection
+        }
+        guard item.count > 0 else {
+            throw VendingMachineError.OutOfStock
+        }
+        guard item.price <= coinsDeposites else {
+            throw VendingMachineError.InsufficientFunds(coinsNeeded: item.price - coinsDeposites)
+        }
+        coinsDeposites -= item.price
+        item.count -= 1
+        inventory[name] = item
+        dispenseSnack(name)
+    }
+}
+let favoriteSnacks = [
+    "Alise": "Chips",
+    "Bob": "Licorice",
+    "Eve": "Pretzels"
+]
+func buyfavoriteSnack(person: String, vendingMachine: VendingMachine) throws {
+    let snackName = favoriteSnacks[person] ?? "Candy Bar"
+    try vendingMachine.vend(itemNamed: snackName)
+}
+
+// Оператор do-catch
+
+var vendingMachine = VendingMachine()
+vendingMachine.coinsDeposites = 8
+do {
+    try buyfavoriteSnack("Alise", vendingMachine: vendingMachine)
+} catch VendingMachineError.InvalidSelection {
+    print("Неправильный выбоор.")
+} catch VendingMachineError.OutOfStock {
+    print("Этого нет в наличии.")
+} catch VendingMachineError.InsufficientFunds(let coinsNeeded) {
+    print("Недостаточно средств. Пожалуйста, внесите ещё \(coinsNeeded) монет(ы).")
+}
+
+
+
+
 
