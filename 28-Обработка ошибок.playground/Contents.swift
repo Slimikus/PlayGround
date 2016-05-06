dba99f9049b1e4a4b4df9d40d940bd7b5d8f317c
Slimikus <slimikus@gmail.com>
2016-05-06 10:37:53 +0300

    28.1 Выбрасывание ошибок

--- 28-Обработка ошибок.playground/Contents.swift
+++ 28-Обработка ошибок.playground/Contents.swift
@@ -1,2 +1,12 @@
 //: 28 - Обработка ошибок
 
+// 28.1 Выбрасывание ошибок
+
+enum VendingMachineError: ErrorType {
+    case InvalidSelection
+    case InsufficientFunds(coinsNeeded: Int)
+    case OutOfStock
+}
+throw VendingMachineError.InsufficientFunds(coinsNeeded: 5)
+
+
