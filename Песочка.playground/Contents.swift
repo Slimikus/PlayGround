47d64589d3b9318d3bb041dc7f80fd02d151196a
Slimikus <slimikus@gmail.com>
2016-06-30 11:31:04 +0300

    создание массива с "0"

--- Песочка.playground/Contents.swift
+++ Песочка.playground/Contents.swift
@@ -9,36 +9,38 @@ let widhtMap = 8
 func Wave(xBegin: Int, _ yBegin: Int, _ xEnd: Int, _ yEnd: Int) {
     var k = 1
     var flag = true
-    var map: [[Int]] = []
+    var map = [[Int]](count: lenghtMap, repeatedValue: [Int](count: widhtMap, repeatedValue: 0))
+    map[xBegin][yBegin] = 1
     while flag {
         flag = false
         for x in 0...lenghtMap - 1 {
             for y in 0...widhtMap - 1 {
-                if map[x][y] == k {
-                    if map[x + 1][y] == 0 {
-                        map[x + 1][y] = k + 1
-                        flag = true
-                    }
-                    if map[x][y - 1] == 0 {
-                        map[x][y - 1] = k + 1
-                        flag = true
-                    }
-                    if map[x][y + 1] == 0 {
-                        map[x][y + 1] = k + 1
-                        flag = true
-                    }
-                    if map[x - 1][y] == 0 {
-                        map[x - 1][y] = k + 1
-                        flag = true
-                    }
-                } else {
-                    if map[xEnd][yEnd] > 0 {
-                        flag = false
+                    print(map[x][y], x, y)
+                    if map[x][y] == k {
+                        if map[x + 1][y] == 0 {
+                            map[x + 1][y] = k + 1
+                            flag = true
+                        }
+                        if map[x][y - 1] == 0 {
+                            map[x][y - 1] = k + 1
+                            flag = true
+                        }
+                        if map[x][y + 1] == 0 {
+                            map[x][y + 1] = k + 1
+                            flag = true
+                        }
+                        if map[x - 1][y] == 0 {
+                            map[x - 1][y] = k + 1
+                            flag = true
+                        }
                     } else {
-                        k = k + 1
+                        if map[xEnd][yEnd] > 0 {
+                            flag = false
+                        } else {
+                            k = k + 1
+                        }
                     }
                 }
-            }
         }
         print(map)
     }
@@ -48,9 +50,13 @@ func Track(xEnd: Int, eEnd: Int) {
     
 }
 
+
 Wave(1, 1, 3, 4)
 var arr: [[Int]] = [[1, 2], [3, 4]]
 arr[1][1]
+
+var arr2 = [Int](count: 5, repeatedValue: 1)
+var arr3 = [[Int]](count: 3, repeatedValue: [Int](count: 7, repeatedValue: 1))
 
 
 
