9fe09995c1692ea9374215232605c001e166893a
Slimikus <slimikus@gmail.com>
2016-07-01 14:46:29 +0300

    РЕШЕНО!

--- ASCII ART.playground/Contents.swift
+++ ASCII ART.playground/Contents.swift
@@ -4,7 +4,7 @@ import UIKit
 
 let L = 4
 let H = 5
-let T = "E"
+let T = "MANHATTAN"
 
 var str = ""
 var arrIntASCII: [Int] = []
@@ -16,36 +16,36 @@ let arrN = ["### ","# # ","# # ","# # ","# # "]
 let arrT = ["### "," #  "," #  "," #  "," #  "]
 let dict = [65: arrA, 69: arrE, 72: arrH, 77: arrM, 78: arrN, 84: arrT]
 
-var arrAnswer: [String] = []
+var arrAnswer1: [[String]] = []
 var arrAnswer2: [String] = []
 
 for i in T.utf16 {
     str = str + String(i) + " "
-    print("i = \(i)")
+    //print("i = \(i)")
     arrIntASCII.append(Int(i))
     
 }
 
-print("Массив i: \(arrIntASCII)")
-print("Длинна массива arrIntASCII = \(arrIntASCII.count)")
-print("Массив arrIntASCII: \(arrIntASCII[0])")//, toStream: &errStream)
+//print("Массив i: \(arrIntASCII)")
+//print("Длинна массива arrIntASCII = \(arrIntASCII.count)")
+print("Массив arrIntASCII: \(arrIntASCII)")//, toStream: &errStream)
 for i in 0...arrIntASCII.count-1 {
     var temp = dict[arrIntASCII[i]]!
-    print(temp)//, toStream: &errStream)
-    arrAnswer.append(dict[arrIntASCII[i]]!)
+    //print("temp= \(temp)")//, toStream: &errStream)
+    ///print("i = \(i)")
+    arrAnswer1.append(temp)
 }
-print(arrAnswer)//, toStream: &errStream)
-
+//print("arrAnswer1= \(arrAnswer1)")
 print(" ")
-var arrInt1 = [["1", "2", "3"], ["1", "2", "3"], ["1", "2", "3"]]
-var arrInt2: [String] = []
-print(arrInt1)
-for j in 0...arrInt1.count-1 {
+for i in 0...H - 1 {
     var str = ""
-    for i in 0...arrInt1[j].count - 1 {
-        str = str + String(arrInt1[i][j])
+    for j in 0...arrAnswer1.count - 1 {
+        str = str + String(arrAnswer1[j][i])
         
     }
-    arrInt2.append(str)
-    print(arrInt2)
+    arrAnswer2.append(str)
+    
+}
+for i in 0...arrAnswer2.count - 1 {
+    print(arrAnswer2[i])
 }
