4186ea917318eb411558760e442768fe1b447e0c
Slimikus <slimikus@gmail.com>
2016-07-11 12:24:30 +0300

    Решение задачи There is no Spoon - Episode 1

--- Песочка.playground/Contents.swift
+++ Песочка.playground/Contents.swift
@@ -1,62 +1,55 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
-// Нахождение кротчайшего пути. Метод волны
 
-let lenghtMap = 3
-let widhtMap = 3
+var arr1 = [[1, 0], [1, 0], [1, 0], [1, 0], [0, 0]]
 
-func Wave(xBegin: Int, _ yBegin: Int, _ xEnd: Int, _ yEnd: Int) {
-    var k = 1
-    var flag = true
-    var map = [[Int]](count: lenghtMap, repeatedValue: [Int](count: widhtMap, repeatedValue: 0))
-    map[xBegin][yBegin] = 1
-    while flag {
-        flag = false
-        for x in 0...lenghtMap - 1 {
-            for y in 0...widhtMap - 1 {
-                    //print(map[x][y], x, y)
-                    if map[x][y] == k {
-                        if map[x + 1][y] == 0 {
-                            map[x + 1][y] = k + 1
-                            flag = true
-                        }
-                        if map[x][y - 1] == 0 {
-                            map[x][y - 1] = k + 1
-                            flag = true
-                        }
-                        if map[x][y + 1] == 0 {
-                            map[x][y + 1] = k + 1
-                            flag = true
-                        }
-                        if map[x - 1][y] == 0 {
-                            map[x - 1][y] = k + 1
-                            flag = true
-                        }
-                    } else {
-                        if map[xEnd][yEnd] > 0 {
-                            flag = false
-                        } else {
-                            k = k + 1
-                        }
-                    }
+for i in 0...arr1.count-1 {
+    print(arr1[i])
+}
+print(" ")
+print("===============")
+print(" ")
+
+var sTemp = ""
+
+for i in 0...arr1.count-1{
+    for j in 0...arr1[i].count-1 {
+        if arr1[i][j] == 1 { // просмотр элементов массива
+            sTemp = sTemp + String(j) + " " + String(i) + " "
+            print("arr1[\(j)][\(i)]= \(arr1[i][j])")
+            var iTemp = i, gBool = false
+            var jTemp = j, vBool = false
+            for var g in jTemp+1...arr1[iTemp].count-1{ // поиск следующего такого же элемента дальше от найденого элемента по горизонтали
+                //print("g= \(g)")
+                if arr1[iTemp][g] == 1 {
+                    print("g = arr1[\(g)][\(iTemp)]= \(arr1[iTemp][g])")
+                    sTemp = sTemp + String(g) + " " + String(iTemp) + " "
+                    gBool = true
+                    break
                 }
+            }
+            if gBool == false {
+                print("bolshe net 1 po goriz")
+                sTemp = sTemp + "-1 -1 "
+            }
+            for v in iTemp+1...arr1.count-1{ // поиск следующего такого же элемента дальше от найденого элемента по вертикали
+                if arr1[v][j] == 1 {
+                    print("v = arr1[\(j)][\(v)]= \(arr1[v][j])")
+                    sTemp = sTemp + String(j) + " " + String(v) + " "
+                    vBool = true
+                    break
+                }
+            }
+            if vBool == false {
+                print("bolshe net 1 po vertic")
+                sTemp = sTemp + "-1 -1 "
+            }
+            print("sTemp= \(sTemp)")
+            sTemp = ""
         }
-        //print(map)
     }
-    
 }
-func Track(xEnd: Int, eEnd: Int) {
-    
-}
-
-
-//Wave(1, 1, 3, 4)
-var arr: [[Int]] = [[1, 2], [3, 4]]
-arr[1][1]
-
-var arr2 = [Int](count: 5, repeatedValue: 1)
-var arr3 = [[Int]](count: 3, repeatedValue: [Int](count: 7, repeatedValue: 1))
 
 
 
